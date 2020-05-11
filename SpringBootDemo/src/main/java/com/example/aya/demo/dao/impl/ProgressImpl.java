194ba281fa7cbae422aeb46bbc7cf2454f404262
Akatsuki_Aya <xiaowen-0526@qq.com>
2020-05-11 20:10:17 +0800

    修改进度findAll根据id排序

--- SpringBootDemo/src/main/java/com/example/aya/demo/dao/impl/ProgressImpl.java
+++ SpringBootDemo/src/main/java/com/example/aya/demo/dao/impl/ProgressImpl.java
@@ -1,10 +1,14 @@
 package com.example.aya.demo.dao.impl;
 
 import com.example.aya.demo.dao.Progress;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * @author Aya
+ */
 public interface ProgressImpl extends JpaRepository<Progress, Long> {
     /**
      * 查询所有进度
@@ -12,4 +16,12 @@ public interface ProgressImpl extends JpaRepository<Progress, Long> {
      */
     @Override
     List<Progress> findAll();
+
+    /**
+     * 查询所有进度 排序
+     * @param sort
+     * @return
+     */
+    @Override
+    List<Progress> findAll(Sort sort);
 }
--- SpringBootDemo/src/main/java/com/example/aya/demo/service/impl/ProgressServiceImpl.java
+++ SpringBootDemo/src/main/java/com/example/aya/demo/service/impl/ProgressServiceImpl.java
@@ -4,8 +4,10 @@ import com.example.aya.demo.dao.Progress;
 import com.example.aya.demo.dao.impl.ProgressImpl;
 import com.example.aya.demo.service.ProgressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,8 +18,13 @@ public class ProgressServiceImpl implements ProgressService {
     @Autowired
     private ProgressImpl progressImpl;
 
+
     @Override
     public List<Progress> findAll() {
-        return progressImpl.findAll();
+        List<Sort.Order> orderList = new ArrayList<>();
+        Sort.Order order = new Sort.Order(Sort.Direction.ASC,"id");
+        orderList.add(order);
+        Sort sort = Sort.by(orderList);
+        return progressImpl.findAll(sort);
     }
 }
