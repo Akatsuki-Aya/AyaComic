9469c283edce9cf150fb494c9dc4a3bdae01a6a6
Akatsuki_Aya <xiaowen-0526@qq.com>
2020-05-18 09:33:49 +0800

    修改密码功能

--- SpringBootDemo/src/main/java/com/example/aya/demo/controller/LoginController.java
+++ SpringBootDemo/src/main/java/com/example/aya/demo/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.aya.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.aya.demo.dao.User;
 import com.example.aya.demo.dao.UserInfo;
 import com.example.aya.demo.dao.impl.UserImpl;
@@ -8,7 +9,9 @@ import com.example.aya.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -51,6 +54,43 @@ public class LoginController {
         return "index2";
     }
 
+    @RequestMapping("/modifyPwd")
+    @ResponseBody
+    public String modifyPwd(String oldPwd, String newPwd) {
+        HttpSession session = request.getSession();
+        Long userId = (Long)session.getAttribute("userId");
+        if (userId!=null){
+        }else {
+            JSONObject result = new JSONObject();
+            result.put("status","error");
+            result.put("msg","无法获取用户id");
+            return result.toJSONString();
+        }
+        User user = userService.findUserById(userId);
+        oldPwd = DigestUtils.md5DigestAsHex(oldPwd.getBytes());
+        if (oldPwd!=null && user.getPassword().equals(oldPwd)){
+        }else {
+            JSONObject result = new JSONObject();
+            result.put("status","error");
+            result.put("msg","旧密码错误");
+            return result.toJSONString();
+        }
+        if (newPwd!=null && !"".equals(newPwd)){
+            newPwd = DigestUtils.md5DigestAsHex(newPwd.getBytes());
+            user.setPassword(newPwd);
+            userService.modify(user);
+        }else {
+            JSONObject result = new JSONObject();
+            result.put("status","error");
+            result.put("msg","新密码格式非法");
+            return result.toJSONString();
+        }
+        JSONObject result = new JSONObject();
+        result.put("status", "success");
+        result.put("msg", "修改成功");
+        return result.toJSONString();
+    }
+
     @RequestMapping("/toLogin")
     public String toLogin() {
         return "index2";
@@ -60,4 +100,8 @@ public class LoginController {
     public String toRegist(){ return "/userPage/regist"; }
     @RequestMapping("/toUploadImgFile")
     public String toUploadImgFile(){return "/userManage/comicUploadManage";}
+    @RequestMapping("/toModifyPwd")
+    public String toModifyPwd(){
+        return "userPage/modifyPwd";
+    }
 }
